91940b7446d7a52de23d02ebf87f7c504dc3e65f
pavanvenkatreddy <104563370+pavanvenkatreddy@users.noreply.github.com>
2025-04-23 16:32:09 -0400

    Supervised Fine-Tuning (SFT) code and model definition

---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f929e71226a73cc1886cf48b8209cb945c776f1684b39d64b63884d04303f631
+size 15830
---
+++ sft.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f15238dfe7d6d64ed5bd85d7cb208fa36e237e10de8265d35df0ba4d4092988
+size 7315
